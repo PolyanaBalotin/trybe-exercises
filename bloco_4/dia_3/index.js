9f7c3a8d9ae760bee89b395f3203646b441177f8
Polyana Balotin <polybalotin@hotmail.com>
2021-05-30 18:49:48 -0300

    teste

--- bloco_4/dia_3/index.js
+++ bloco_4/dia_3/index.js
@@ -1,3 +1,4 @@
+//faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n 
 let n = 5;
 let asterisco = '*';
 let inputLine = '';
@@ -7,4 +8,33 @@ for (let index = 0; index < n; index += 1) {
 };
 for (let index = 0; index < n; index += 1) {
   console.log(inputLine);
+};
+
+//imprima um triângulo retângulo com 5 asteriscos de base.
+let n = 5;
+let asterisco = '*';
+let inputLine = '';
+
+for (let index = 0; index <= n; index += 1) {
+  console.log(inputLine);
+  inputLine = inputLine + asterisco;
+};
+
+//Agora inverta o lado do triângulo
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+let inputPosition = n;
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+    if (columnIndex < inputPosition) {
+      inputLine = inputLine + ' ';
+    } else {
+      inputLine = inputLine + symbol;
+    }
+  }
+  console.log(inputLine);
+  inputLine = '';
+  inputPosition -= 1;
 };
