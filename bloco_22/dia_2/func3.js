245c86bbf918d769e6564aea614e9e3a04cc7ba2
Polyana Balotin <polybalotin@hotmail.com>
2022-01-14 21:34:39 -0300

    Fluxo Assincrono

---
+++ bloco_22/dia_2/func3.js
@@ -0,0 +1,11 @@
+const fs = require('fs').promises;
+
+async function filterSimpsons() {
+  const simpsons = await fs.readFile('./simpsons.json', 'utf8')
+  .then((fileContent) => JSON.parse(fileContent));
+
+  const newArray = simpsons.filter(simpson => simpson.id !== '10' && simpson.id !== '6');
+  await fs.writeFile('simpsons.json', JSON.stringify(newArray));
+}
+
+filterSimpsons();
